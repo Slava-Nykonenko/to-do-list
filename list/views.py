8a7d0070bc6a9e178a9d8a525d32e85629c98972
Viacheslav Nykonenko <v.nykonenko@gmail.com>
2025-09-19 23:54:52 +0100

    created models and views for Task and Tag

--- list/views.py
+++ list/views.py
@@ -1,3 +1,83 @@
-from django.shortcuts import render
+# Create your views here.
+from typing import Any
+
+from django.http import (
+    HttpResponseRedirect,
+    HttpRequest
+)
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
+from django.views import generic
+
+from list.forms import (
+    TaskForm,
+    TagForm
+)
+from list.models import (
+    Task,
+    Tag
+)
+
 
 # Create your views here.
+class TaskListView(generic.ListView):
+    model = Task
+    paginate_by = 10
+    queryset = Task.objects.all().prefetch_related("tags")
+
+
+class TaskDetailView(generic.DetailView):
+    model = Task
+    queryset = Task.objects.all().prefetch_related("tags")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    paginate_by = 10
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("list:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("list:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("list:tag-list")
+
+
+class TagDetailView(generic.DetailView):
+    model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    form_class = TagForm
+    success_url = reverse_lazy("list:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    form_class = TagForm
+    success_url = reverse_lazy("list:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("list:tag-list")
+
+
+def changestatus(request: HttpRequest, pk: int) -> Any | None:
+    task = get_object_or_404(Task, pk=pk)
+    task.done = not task.done
+    task.save()
+    return HttpResponseRedirect(
+        reverse_lazy("list:task-list")
+    )
